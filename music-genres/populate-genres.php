c4cb984ea5884d47338c2589a52a3f979e28fdb8
Remy <tmni0p20>
2016-12-20 10:18:07 +0100

    Updated list of genres.

--- music-genres/populate-genres.php
+++ music-genres/populate-genres.php
@@ -1,6 +1,6 @@
 <?php
 // query for Wikidata
-$query = 'https://query.wikidata.org/bigdata/namespace/wdq/sparql?query=SELECT%20DISTINCT%20%3Fgenre%20%3Flabel%0AWHERE%20%7B%0A%20%20%20%20%20%20%20%20%7B%20%3Fgenre%20%3Chttp%3A%2F%2Fwww.wikidata.org%2Fprop%2Fdirect%2FP31%3E%20%3Chttp%3A%2F%2Fwww.wikidata.org%2Fentity%2FQ188451%3E%20%7D%0A%20%20UNION%20%7B%20%3Fgenre%20%3Chttp%3A%2F%2Fwww.wikidata.org%2Fprop%2Fdirect%2FP31%3E%20%3Chttp%3A%2F%2Fwww.wikidata.org%2Fentity%2FQ862597%3E%20%7D%20.%0A%20%20OPTIONAL%20%7B%20%3Fgenre%20rdfs%3Alabel%20%3Flabel%20.%20FILTER%20%28lang%28%3Flabel%29%20%3D%20%27en%27%29%20%7D%0A%7D';
+$query = 'https://query.wikidata.org/bigdata/namespace/wdq/sparql?query=PREFIX%20wp%3A%20%3Chttp%3A%2F%2Fwww.wikidata.org%2Fprop%2Fdirect%2F%3E%20%0APREFIX%20we%3A%20%3Chttp%3A%2F%2Fwww.wikidata.org%2Fentity%2F%3E%20%0ASELECT%20DISTINCT%20%3Fgenre%20%3Flabel%20%20WHERE%20%7B%0A%20%20%7B%20%3Fgenre%20wp%3AP31%20%7C%20wp%3AP279%20we%3AQ862597%20%7D%20UNION%20%7B%20%3Fgenre%20wp%3AP31%20%7C%20wp%3AP279%20we%3AQ188451%20%7D%20%20.%0A%20%20OPTIONAL%20%7B%20%3Fgenre%20rdfs%3Alabel%20%3Flabel%20.%20FILTER%20%28lang%28%3Flabel%29%20%3D%20%27en%27%29%20%7D%0A%7D';
 
 // create stream
 $opts = array(
